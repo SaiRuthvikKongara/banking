0996fc7f6d72596faf1b9043d2df4b195cccee50
K.Sai Ruthvik <ksairuthvik2@gmail.com>
2024-10-07 15:24:13 +0530

    Fix line endings

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    
+        <h1>Sign In</h1>
+   
+
+  )
+}
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    
+      <h1>SignUp Page</h1>
+   
+    
+  )
+}
+
+export default SignUp;
