1efaa1f5f12bfe0a851066b65ba41277ec0f03f1
K.Sai Ruthvik <ksairuthvik2@gmail.com>
2024-10-08 10:02:54 +0530

    implent operations

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,28 @@
+
+import Sidebar from "@/components/Sidebar";
 import React from "react";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const loggedIn = {firstName: 'Sai Ruthvik', lastName:'Kongara'};
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+
+        <div className="flex size-full flex-col">
+            <div className="root-layout">
+                <Image src="/icons/logo.svg" width={30} height={30} alt="logo"/>
+                <div>
+                    <MobileNav user={loggedIn}/>
+                </div>
+            </div>
         {children}
+        </div>
+        
     </main>
   );
 }
